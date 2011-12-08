e55e3fb1edceb61c289817a5caba9c0be2e405d3
Your Name <paul.fuller.uk@googlemail.com>
2011-12-08 13:27:51 +0000

    added idea support & updated README

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,4 +3,7 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val akkaRepo   = "Akka Repo" at "http://akka.io/repository"
   val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.2"
+  
+  val ideaSbtRepo= "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+  val sbtIdeaPlugin = "com.github.mpeltonen" %% "sbt-idea" % "0.4.0"
 }
